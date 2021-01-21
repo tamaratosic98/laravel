1ef6de40fbb0ca44f5f6f6d7a0504ff1bfe4f147
tamaratosic98 <tosict98@gmail.com>
2021-01-21 22:43:41 +0100

    Migracija (pravljenje articles) + seeder prvi deo

--- laraveldomaci/database/seeders/DatabaseSeeder.php
+++ laraveldomaci/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(ArticlesTableSeeder::class);//pozivamo seeder za articles
     }
 }
