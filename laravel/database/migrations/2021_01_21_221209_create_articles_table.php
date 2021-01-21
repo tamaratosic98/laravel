0ed47cd836d51aa46f1636292261d5bfe820a4bd
tamaratosic98 <tosict98@gmail.com>
2021-01-21 23:12:29 +0100

    instalacija laravela

--- laravel/database/migrations/2021_01_21_221209_create_articles_table.php
+++ laravel/database/migrations/2021_01_21_221209_create_articles_table.php
@@ -15,8 +15,6 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('titile');
-            $table->text('body');
             $table->timestamps();
         });
     }
