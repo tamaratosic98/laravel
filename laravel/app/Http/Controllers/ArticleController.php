416753d49c53b0d02f17919fc7b7b46a35e6fe7c
tamaratosic98 <tosict98@gmail.com>
2021-01-22 00:17:05 +0100

    API za articles

---
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Article;
+use App\Http\Resources\Article as ArticleResource;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $articles = Article::paginate(15);
+        //Return the collection of articles as a resource
+        return ArticleResource::collection($articles);
+
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //for creating and updating
+        $article=$request->isMethod('put')? Article::findOrFail($request->article_id):new Article;
+        $article->id=$request->input('article_id');
+        $article->title=$request->input('title');
+        $article->body=$request->input('body');
+        if($article->save()){
+         return new ArticleResource($article);
+
+        }
+
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $article = Article::findOrFail($id);
+        return new ArticleResource($article);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $article = Article::findOrFail($id);
+        //return the single article as a resource
+        if($article->delete()){
+            return new ArticleResource($article);
+        }
+
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/articles',[ArticleController::class, 'index']);
+Route::get('/article/{id}',[ArticleController::class, 'show']);
+Route::post('/article',[ArticleController::class, 'store']);
+Route::put('/article',[ArticleController::class, 'store']);
+Route::delete('/article/{id}',[ArticleController::class, 'destroy']);
