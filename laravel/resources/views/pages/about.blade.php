a3adbba7065285e9c83d2f9ac02a188fb9f83d67
tamaratosic98 <tosict98@gmail.com>
2021-01-27 18:19:45 +0100

    about us tekst

--- laravel/resources/views/pages/about.blade.php
+++ laravel/resources/views/pages/about.blade.php
@@ -2,5 +2,6 @@
 @extends('layouts.app')
 @section('content')
     <h1>{{$title}}</h1>
-    <h1>About strana</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi explicabo voluptate a ipsum magnam ullam laborum. Ab alias officiis cum nisi libero, et voluptates exercitationem dolorum blanditiis rem perferendis natus.</h1>
+    <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit ut neque, officia eum adipisci non obcaecati modi veniam necessitatibus aliquam sunt nemo dicta explicabo magni consequatur, soluta molestiae maiores similique?</h3>
 @endsection
